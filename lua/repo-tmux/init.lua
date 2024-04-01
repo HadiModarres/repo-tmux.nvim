f15564ade3fc82bbf1062ea43dd6e499cffdd419
Hadi Modarres <modarres.zadeh@gmail.com>
2024-04-01 23:35:08 +1100

    added commands

--- lua/repo-tmux/init.lua
+++ lua/repo-tmux/init.lua
@@ -6,19 +6,9 @@ local function get_last_element(inputstr)
 	return lastElement
 end
 
-local M = {
-	setup = function(opts)
-		print("setup")
-		print(opts)
-	end,
-	open = function() end,
-}
+local M = {}
 
 local terminal_state = {}
-
-local function on_win_close()
-	terminal_state.open_win_handle = nil
-end
 
 local function create_window()
 	vim.api.nvim_command("botright split")
@@ -27,7 +17,6 @@ local function create_window()
 	vim.api.nvim_create_autocmd("WinClosed", {
 		pattern = tostring(win_handle),
 		callback = function(ev)
-			print("callback called")
 			terminal_state.open_win_handle = nil
 		end,
 	})
@@ -37,14 +26,13 @@ local function kill_window()
 	vim.api.nvim_win_close(terminal_state.open_win_handle, true)
 end
 
-local function kill_term_buffer()
-	vim.api.nvim_buf_delete(terminal_state.buf_handle, { force = false })
+local function is_window_open()
+	return terminal_state.open_win_handle ~= nil
 end
 
 local function create_term_buffer()
-	local buf_handle = vim.api.nvim_create_buf(false, true)
-
 	local oldHandle = terminal_state.buf_handle
+	local buf_handle = vim.api.nvim_create_buf(false, true)
 
 	terminal_state.buf_handle = buf_handle
 
@@ -55,15 +43,12 @@ local function create_term_buffer()
 
 	local lastElement = get_last_element(git_root)
 	local tmux_session_name = string.format('"%s (%s)"', lastElement, git_root)
-	print(tmux_session_name)
 	local tmux_cmd = "cd " .. git_root .. " && tmux new-session -A -s " .. tmux_session_name
 
 	vim.api.nvim_win_set_buf(terminal_state.open_win_handle, buf_handle)
 
-	-- vim.api.nvim_set_current_buf(buf_handle)
 	vim.api.nvim_set_current_win(terminal_state.open_win_handle)
 	vim.fn.termopen(tmux_cmd, { cwd = git_root })
-	-- vim.api.nvim_command(":terminal " .. tmux_cmd)
 
 	vim.cmd(":startinsert")
 
@@ -73,69 +58,30 @@ local function create_term_buffer()
 end
 
 function M.open()
-	if terminal_state.open_win_handle then
-		-- kill_term_buffer()
-		-- kill_window()
-		-- M.open()
-		create_term_buffer()
-	else
+	if is_window_open() == false then
 		create_window()
-		create_term_buffer()
 	end
+	create_term_buffer()
 end
 
 function M.close()
 	kill_window()
 end
 
--- function M.open()
--- 	local file_path = vim.fn.expand("%:p")
---
--- 	local file_dir = vim.fn.fnamemodify(file_path, ":h")
---
--- 	local git_root_cmd = "git -C " .. file_dir .. " rev-parse --show-toplevel"
--- 	local git_root = vim.fn.system(git_root_cmd):gsub("\n", "")
---
--- 	-- local git_root = vim.fn.system(git_root_cmd):gsub("\n", "")
--- 	--
--- 	local lastElement = get_last_element(git_root)
---
--- 	local tmux_session_name = string.format('"%s (%s)"', lastElement, git_root)
---
--- 	print(tmux_session_name)
---
--- 	local tmux_cmd = "cd " .. git_root .. " && tmux new-session -A -s " .. tmux_session_name
---
--- 	-- local tmux_cmd = "tmux new-session -A -s " .. git_root
---
--- 	-- vim.api.nvim_command(":terminal " .. tmux_cmd)
--- 	-- vim.cmd(":startinsert")
---
--- 	vim.api.nvim_command("botright split")
---
--- 	local window = vim.api.nvim_get_current_win()
--- 	local bufnr = vim.api.nvim_create_buf(false, false)
---
--- 	terminal_state[bufnr] = {
--- 		bufnr = bufnr,
--- 		window = window,
--- 	}
---
--- 	self.current_buf = bufnr
---
--- 	print(self.current_buf)
---
--- 	vim.api.nvim_win_set_buf(window, bufnr)
---
--- 	vim.api.nvim_open_term(bufnr, {})
---
--- 	vim.api.nvim_command(":terminal " .. tmux_cmd)
---
--- 	vim.cmd(":startinsert")
---
--- 	-- vim.fn.termopen({ tmux_cmd }, { cwd = git_root })
---
--- 	-- current_buf = vim.api.nvim_create_buf(true, false)
--- end
+local function setupCommands()
+	local command = vim.api.nvim_create_user_command
+
+	command("RepoTmuxOpen", function()
+		M.open()
+	end, {})
+
+	command("RepoTmuxClose", function()
+		M.close()
+	end, {})
+end
+
+function M.setup()
+	setupCommands()
+end
 
 return M
